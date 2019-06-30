b6a7ed8d93a571a4bc84a316b060bdbf14e700fa
is8200 <is8200@naver.com>
2019-06-30 19:11:14 +0900

    oh

---
+++ Bamsongi/Assets/BamsongiController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+}
---
+++ ClimbCloud/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2d;
+    float jumpForce = 780.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2d = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2d.velocity.y == 0)
+        {
+            this.animator.SetTrigger("JumpTrigger");
+            this.rigid2d.AddForce(transform.up * this.jumpForce);
+
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2d.velocity.x);
+
+        if(speedx < this.maxWalkSpeed)
+        {
+           
+            //여기에 기기마다 속도차이를 감안하려면 Time.deltaTime 곱해줌
+            this.rigid2d.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2.0f;
+
+        if(transform.position.y < -10)
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("골");
+        SceneManager.LoadScene("ClearScene");
+    }
+
+}
